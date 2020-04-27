ad8219d216495b3323c130e98e2e837df1e71cce
Azamat Galimzhanov <azamat@galimzhanov.com>
2020-04-27 16:46:22 +0600

    Remve nock

--- tests/setup.js
+++ tests/setup.js
@@ -1,11 +1,6 @@
-import axios from 'axios'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import { init } from '@/init'
 
 Vue.use(Vuetify)
 init()
-// Fixes "Cross origin http://localhost forbidden"
-// Maybe it's better to use moxios instead of nock
-// to avoid using require
-axios.defaults.adapter = require('axios/lib/adapters/http')
